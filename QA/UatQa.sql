265820d3172762f907db9a2ae3ebeef6c4393aa8
haileyhahnnnn <49739147+haileyhahnnnn@users.noreply.github.com>
2025-03-12 12:37:21 -0700

    Update UatQa.sql

--- QA/UatQa.sql
+++ QA/UatQa.sql
@@ -3,3 +3,9 @@ SELECT *
 FROM 'dataset.table.name' 
 Where FieldNameHere is not null 
 LIMIT 100; -- change limit as needed 
+
+-- Drop a view in GCP && then create new one
+Drop View if Exsists viewDataSetName.ViewName;
+
+Create view viewDataSetName.ViewName as 
+(SELECT * FROM 'DataSet.View.Name' LIMIT 1000)
